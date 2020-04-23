87b6600aaa83236853baaed7e1eeee54402220ba
AntoineMurry <antoine.victor.murry@gmail.com>
2020-04-23 14:50:56 +0200

    add files for installation

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages
+from setuptools import setup
+
+# Python version used for development: 3.6.7
+
+requirements = """
+pip>=9
+setuptools>=26
+wheel>=0.29
+pytest
+coverage
+flake8
+matplotlib
+numpy
+"""
+
+setup(name='MLP_from_scratch',
+      setup_requires=['setuptools_scm'],
+      use_scm_version={'write_to': 'MLP_from_scratch/version.txt'},
+      description="package with MLP coded from numpy",
+      packages=find_packages(),
+      test_suite = 'tests',
+      # include_package_data: to install data from MANIFEST.in
+      include_package_data=True,
+      zip_safe=False)
